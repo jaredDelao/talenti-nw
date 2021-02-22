764f025eb5625827a831e68a9ecf13c2916a4f1a
jaredDelao <jaredhm07@gmail.com>
2021-02-22 12:42:30 -0600

    Actualización url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   urlGet: 'http://34.234.225.159/bkService.php',
   // urlProd: 'http://ec2-54-210-170-110.compute-1.amazonaws.com/WebTalenti/requestsrvrTalenti.php',
-  urlProd: 'https://backtalenti.automatizate.mx/WebTalenti/requestsrvrTalenti.php',
+  urlProd: 'backend.talenti.com.mx/WebTalenti/requestsrvrTalenti.php',
   // urlArchivo: 'http://ec2-54-210-170-110.compute-1.amazonaws.com/WebTalenti/Subearchivo.php'
-  urlArchivo: 'https://backtalenti.automatizate.mx/WebTalenti/Subearchivo.php',
+  urlArchivo: 'backend.talenti.com.mx/WebTalenti/Subearchivo.php',
   urlImage: 'http://ec2-54-210-170-110.compute-1.amazonaws.com/WebTalenti'
 
 };
