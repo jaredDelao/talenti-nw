2ae45ea41d4beb0562296a7f603d183a38764bec
jaredDelao <jaredhm07@gmail.com>
2021-10-01 12:06:31 -0500

    Ajustes

--- src/app/shared/docs/tiposDictamen.ts
+++ src/app/shared/docs/tiposDictamen.ts
@@ -1,20 +1,14 @@
+export const clienteNormal = [
+  { id: 1, nombre: "EN PROCESO" },
+  { id: 2, nombre: "RECOMENDADO" },
+  { id: 3, nombre: "NO RECOMENDADO" },
+  { id: 4, nombre: "RECOMENDADO CON RESERVA" },
+];
 
-
-    export const clienteNormal =  [
-        {id: 1, nombre: 'EN PROCESO'},
-        {id: 2, nombre: 'RECOMENDADO'},
-        {id: 3, nombre: 'NO RECOMENDADO'},
-        {id: 4, nombre: 'RECOMENDADO CON RESERVA' }
-    ];
-    
-    export const clienteGNP = [
-        {id: 10, nombre: 'EN PROCESO'},
-        {id: 11, nombre: 'SR'},
-        {id: 12, nombre: 'R1'},
-        {id: 13, nombre: 'R2' },
-        {id: 14, nombre: 'R3' }
-    ];
-
-    // export const clienteGNP;
-    // export const clienteNormal;
-
+export const clienteGNP = [
+  { id: 10, nombre: "EN PROCESO" },
+  { id: 11, nombre: "SR" },
+  { id: 12, nombre: "R1" },
+  { id: 13, nombre: "R2" },
+  { id: 14, nombre: "R3" },
+];
